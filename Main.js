8345d0ddb3bd96b0c6f756d519c86fbe5baad426
DAN1ROCK <goletsdan@gmail.com>
2022-06-26 19:17:13 +0300

    formatted scripts with Prettier

--- Main.js
+++ Main.js
@@ -1,128 +1,171 @@
-'use strict';
+"use strict";
 
-const cvs = document.getElementById('canvas');
-const ctx = cvs.getContext('2d');
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
 
 cvs.width = window.innerWidth;
 cvs.height = window.innerHeight;
 
 const canvas = {
-    width: cvs.width,
-    height: cvs.height
-}
+  width: cvs.width,
+  height: cvs.height,
+};
 
 const background = new Image();
-const player = new Image(); 
+const player = new Image();
 const bullet = new Image();
 const particles = new Image();
 const greyParticles = new Image();
 const spawner = new Image();
 
-background.src = 'Textures/Floor.png';
-player.src = 'Textures/Character.png';
-bullet.src = 'Textures/bullet.png';
-particles.src = 'Textures/WhiteParticles.png';
-greyParticles.src = 'Textures/GreyParticles.png';
-spawner.src = 'Textures/spawner.png'
+background.src = "Textures/Floor.png";
+player.src = "Textures/Character.png";
+bullet.src = "Textures/bullet.png";
+particles.src = "Textures/WhiteParticles.png";
+greyParticles.src = "Textures/GreyParticles.png";
+spawner.src = "Textures/spawner.png";
 
 const key = {
-    up: false,
-    down: false,
-    left: false,
-    right: false
-}
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+};
 
 const mousePos = {
-    X: -1,
-    Y: -1
+  X: -1,
+  Y: -1,
+};
+
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
+
+function keyDown(event) {
+  if (event.code == "KeyW") key.up = true;
+  if (event.code == "KeyS") key.down = true;
+  if (event.code == "KeyA") key.left = true;
+  if (event.code == "KeyD") key.right = true;
+  if (event.code == "KeyE")
+    bullets.push(
+      new Bullet(
+        calculateAngle(xPos, yPos, mousePos.X, mousePos.Y) + toRadians(90),
+        getCenterX(xPos, player, 0.4),
+        getCenterY(yPos, player, 0.4),
+        2,
+        bullets.length
+      )
+    );
+  if (event.code == "KeyQ") {
+    playerSpeed *= 5;
+    setTimeout(resetPlayerSpeed, (50 / timeCoef) * hzCoef);
+  }
 }
 
-document.addEventListener('keydown', keyDown);
-document.addEventListener('keyup', keyUp);
-
-function keyDown(event){
-    if(event.code == 'KeyW') key.up = true;
-    if(event.code == 'KeyS') key.down = true;
-    if(event.code == 'KeyA') key.left = true;
-    if(event.code == 'KeyD') key.right = true;
-    if(event.code == 'KeyE') bullets.push(new Bullet(
-        calculateAngle(xPos, yPos, mousePos.X, mousePos.Y) + toRadians(90), 
-        getCenterX(xPos, player, 0.4), getCenterY(yPos, player, 0.4), 2, bullets.length)
-        )
-    if(event.code == 'KeyQ') {
-        playerSpeed *= 5;
-        setTimeout(resetPlayerSpeed, 50 / timeCoef * hzCoef);
-    }
+function keyUp(event) {
+  if (event.code == "KeyW") key.up = false;
+  if (event.code == "KeyS") key.down = false;
+  if (event.code == "KeyA") key.left = false;
+  if (event.code == "KeyD") key.right = false;
 }
 
-function keyUp(event){
-    if(event.code == 'KeyW') key.up = false;
-    if(event.code == 'KeyS') key.down = false;
-    if(event.code == 'KeyA') key.left = false;
-    if(event.code == 'KeyD') key.right = false;
-}
+const drawImage = (
+  ctx,
+  img,
+  x,
+  y,
+  angle = 0,
+  scale = 1,
+  absoluteCenter = false
+) => {
+  let xCenterShear = absoluteCenter ? (img.width * scale) / 2 : 0;
+  let yCenterShear = absoluteCenter ? (img.height * scale) / 2 : 0;
+  ctx.save();
+  ctx.translate(
+    x + (img.width * scale) / 2 - xCenterShear,
+    y + (img.height * scale) / 2 - yCenterShear
+  );
+  ctx.rotate(angle);
+  ctx.translate(
+    -x - (img.width * scale) / 2 + xCenterShear,
+    -y - (img.height * scale) / 2 + yCenterShear
+  );
+  ctx.drawImage(
+    img,
+    x - xCenterShear,
+    y - yCenterShear,
+    img.width * scale,
+    img.height * scale
+  );
+  ctx.restore();
+};
 
-const drawImage = (ctx, img, x, y, angle = 0, scale = 1, absoluteCenter = false) => {
-    let xCenterShear = absoluteCenter ? img.width * scale / 2 : 0;
-    let yCenterShear = absoluteCenter ? img.height * scale / 2 : 0;
-    ctx.save();
-    ctx.translate(x + img.width * scale / 2 - xCenterShear, y +  img.height * scale / 2 - yCenterShear);
-    ctx.rotate(angle);
-    ctx.translate(- x - img.width * scale / 2 + xCenterShear, - y -  img.height * scale / 2 + yCenterShear);
-    ctx.drawImage(img, x - xCenterShear, y - yCenterShear, img.width * scale, img.height * scale);
-    ctx.restore();
-}
-
-document.onmousemove = function(event) {
-    mousePos.X = event.pageX;
-    mousePos.Y = event.pageY;
-}
+document.onmousemove = function (event) {
+  mousePos.X = event.pageX;
+  mousePos.Y = event.pageY;
+};
 
 const objectDestroyed = (index, objectArray) => {
-    for(let i = index; i < objectArray.length; ++i) objectArray[i].index -= 1;
-}
+  for (let i = index; i < objectArray.length; ++i) objectArray[i].index -= 1;
+};
 
-const calculateAngle = (x1, y1, x2, y2, cursor = true) => { 
-    const centerX1 = getCenterX(x1, player, 0.4);
-    const centerY1 = getCenterY(y1, player, 0.4);
-    const centerX2 = cursor ? x2 : getCenterX(x2, player, 0.4);
-    const centerY2 = cursor ? y2 : getCenterY(y2, player, 0.4);
-    const pi = (centerY1-centerY2) >= 0 ? Math.PI : 0;
-    return -Math.atan((centerX1 - centerX2) / (centerY1 - centerY2)) + pi;
-}
+const calculateAngle = (x1, y1, x2, y2, cursor = true) => {
+  const centerX1 = getCenterX(x1, player, 0.4);
+  const centerY1 = getCenterY(y1, player, 0.4);
+  const centerX2 = cursor ? x2 : getCenterX(x2, player, 0.4);
+  const centerY2 = cursor ? y2 : getCenterY(y2, player, 0.4);
+  const pi = centerY1 - centerY2 >= 0 ? Math.PI : 0;
+  return -Math.atan((centerX1 - centerX2) / (centerY1 - centerY2)) + pi;
+};
 
 const spawnEnemy = () => {
-    /*const side = Math.floor(Math.random() * 4); 
+  /*const side = Math.floor(Math.random() * 4); 
     if(side == 0) enemies.push(new Enemy(-50, Math.random() * canvas.height, 0.2, enemies.length));
     else if(side == 1) enemies.push(new Enemy(canvas.width + 20, Math.random() * canvas.height, 0.2, enemies.length));
     else if(side == 2) enemies.push(new Enemy(Math.random() * canvas.width, -50, 0.2, enemies.length));
     else if(side == 3) enemies.push(new Enemy(Math.random() * canvas.width, canvas.height + 20, 0.2, enemies.length));*/
-    enemies.push(new Enemy(Math.random() * canvas.width, Math.random() * canvas.height, 0.2, enemies.length));
-}
+  enemies.push(
+    new Enemy(
+      Math.random() * canvas.width,
+      Math.random() * canvas.height,
+      0.2,
+      enemies.length
+    )
+  );
+};
 
 const explosion = (x, y, radius, maxScale, particleCount) => {
-    for(let i = 0; i < particleCount; ++i){
-        const spawnX = x + Math.random() * radius * 2 - radius;
-        const spawnY = y + Math.random() * radius * 2 - radius;
-        allParticles.push(new Particles(
-            spawnX, 
-            spawnY, 
-            Math.random() * (maxScale / 2) + maxScale / 2, 
-            allParticles.length, 
-            greyParticles, 
-            calculateAngle(x, y, spawnX, spawnY, false) + toRadians(90), 
-            Math.random() * 0.2, 0));
-    }
-}
+  for (let i = 0; i < particleCount; ++i) {
+    const spawnX = x + Math.random() * radius * 2 - radius;
+    const spawnY = y + Math.random() * radius * 2 - radius;
+    allParticles.push(
+      new Particles(
+        spawnX,
+        spawnY,
+        Math.random() * (maxScale / 2) + maxScale / 2,
+        allParticles.length,
+        greyParticles,
+        calculateAngle(x, y, spawnX, spawnY, false) + toRadians(90),
+        Math.random() * 0.2,
+        0
+      )
+    );
+  }
+};
 
 const resetPlayerSpeed = () => {
-    playerSpeed = 0.5;
-}
+  playerSpeed = 0.5;
+};
 
-const getCenterX = (x, img, scale) => { return x + img.width * scale / 2; }
-const getCenterY = (y, img, scale) => { return y + img.height * scale / 2; }
+const getCenterX = (x, img, scale) => {
+  return x + (img.width * scale) / 2;
+};
+const getCenterY = (y, img, scale) => {
+  return y + (img.height * scale) / 2;
+};
 
-const toRadians = (angle) => { return angle * (Math.PI / 180) }
+const toRadians = (angle) => {
+  return angle * (Math.PI / 180);
+};
 
 let xPos = canvas.width / 2;
 let yPos = canvas.height / 2;
@@ -132,31 +175,37 @@ let allParticles = [];
 let enemies = [];
 
 const main = () => {
-    for(let x = 0; x < canvas.width; x += 150){
-        for(let y = 0; y < canvas.height; y += 150){
-            ctx.drawImage(background, x, y, 150, 150);
-        }
+  for (let x = 0; x < canvas.width; x += 150) {
+    for (let y = 0; y < canvas.height; y += 150) {
+      ctx.drawImage(background, x, y, 150, 150);
     }
+  }
 
-    if(key.up) yPos -= playerSpeed * hzCoef * timeCoef;
-    if(key.down) yPos += playerSpeed * hzCoef * timeCoef;
-    if(key.left) xPos -= playerSpeed * hzCoef * timeCoef;
-    if(key.right) xPos += playerSpeed * hzCoef * timeCoef;
+  if (key.up) yPos -= playerSpeed * hzCoef * timeCoef;
+  if (key.down) yPos += playerSpeed * hzCoef * timeCoef;
+  if (key.left) xPos -= playerSpeed * hzCoef * timeCoef;
+  if (key.right) xPos += playerSpeed * hzCoef * timeCoef;
 
-    if(key.up || key.down || key.right || key.left) {
-        if(timeCoef < 1) timeCoef += 0.02 * hzCoef * timeCoef;
-    } 
-    else if(timeCoef > 0.2) timeCoef -= 0.02 * hzCoef * timeCoef;
+  if (key.up || key.down || key.right || key.left) {
+    if (timeCoef < 1) timeCoef += 0.02 * hzCoef * timeCoef;
+  } else if (timeCoef > 0.2) timeCoef -= 0.02 * hzCoef * timeCoef;
 
-    if(timeCoef > 1) timeCoef = 1;
-    if(timeCoef < 0.2) timeCoef = 0.2;
+  if (timeCoef > 1) timeCoef = 1;
+  if (timeCoef < 0.2) timeCoef = 0.2;
 
-    for(let i = 0; i < allParticles.length; ++i) allParticles[i].particlesMain();
-    for(let i = 0; i < bullets.length; ++i) bullets[i].bulletMain();
-    for(let i = 0; i < enemies.length; ++i) enemies[i].enemyMain();
+  for (let i = 0; i < allParticles.length; ++i) allParticles[i].particlesMain();
+  for (let i = 0; i < bullets.length; ++i) bullets[i].bulletMain();
+  for (let i = 0; i < enemies.length; ++i) enemies[i].enemyMain();
 
-    drawImage(ctx, player, xPos, yPos, calculateAngle(xPos, yPos, mousePos.X, mousePos.Y) + toRadians(90), 0.4);
-}
+  drawImage(
+    ctx,
+    player,
+    xPos,
+    yPos,
+    calculateAngle(xPos, yPos, mousePos.X, mousePos.Y) + toRadians(90),
+    0.4
+  );
+};
 
 const hz60 = 1000 / 60;
 const hz144 = 1000 / 144;
--- Objects.js
+++ Objects.js
@@ -1,134 +1,183 @@
-'use strict';
+"use strict";
 
 class Bullet {
-    constructor(angle, x, y, speed, index, isEnemy = false) {
-        this.x = x;
-        this.y = y;
-        this.angle = angle;
-        this.speed = speed;
-        this.index = index;
-        this.timer = 0;
-        this.isEnemy = isEnemy;
+  constructor(angle, x, y, speed, index, isEnemy = false) {
+    this.x = x;
+    this.y = y;
+    this.angle = angle;
+    this.speed = speed;
+    this.index = index;
+    this.timer = 0;
+    this.isEnemy = isEnemy;
+  }
+
+  bulletMain() {
+    drawImage(ctx, bullet, this.x, this.y, this.angle, 0.1);
+    this.x += this.speed * Math.cos(this.angle) * hzCoef * timeCoef;
+    this.y += this.speed * Math.sin(this.angle) * hzCoef * timeCoef;
+
+    if (
+      this.x < -10 ||
+      this.x > canvas.width + 10 ||
+      this.y < -10 ||
+      this.y > canvas.height + 10
+    ) {
+      this.destroy();
     }
 
-    bulletMain() {
-        drawImage(ctx, bullet, this.x, this.y, this.angle, 0.1);
-        this.x += this.speed * Math.cos(this.angle) * hzCoef * timeCoef;
-        this.y += this.speed * Math.sin(this.angle) * hzCoef * timeCoef;
-
-        if(this.x < -10 || this.x > canvas.width + 10 || this.y < -10 || this.y > canvas.height + 10){
-            this.destroy();
-        }
-
-        if(this.timer <= 0) {
-            allParticles.push(new Particles(getCenterX(this.x, bullet, 0.1), getCenterY(this.y, bullet, 0.1), 0.1, allParticles.length, particles));
-            this.timer = 1;
-        }
-
-        this.timer -= 0.5 * hzCoef * timeCoef;
+    if (this.timer <= 0) {
+      allParticles.push(
+        new Particles(
+          getCenterX(this.x, bullet, 0.1),
+          getCenterY(this.y, bullet, 0.1),
+          0.1,
+          allParticles.length,
+          particles
+        )
+      );
+      this.timer = 1;
     }
 
-    destroy() {
-        explosion(getCenterX(this.x, bullet, 0.1), getCenterY(this.y, bullet, 0.1), 3, 0.1, 5);
-        bullets.splice(this.index, 1);
-        objectDestroyed(this.index, bullets);
-    }
+    this.timer -= 0.5 * hzCoef * timeCoef;
+  }
+
+  destroy() {
+    explosion(
+      getCenterX(this.x, bullet, 0.1),
+      getCenterY(this.y, bullet, 0.1),
+      3,
+      0.1,
+      5
+    );
+    bullets.splice(this.index, 1);
+    objectDestroyed(this.index, bullets);
+  }
 }
 
 class Particles {
-    constructor(x, y, scale, index, img, angle = 0, speed = 0, spawnTimer = 0.8) {
-        this.scale = scale;
-        this.x = x;
-        this.y = y;
-        this.spawnTimer = spawnTimer;
-        this.index = index;
-        this.img = img;
-        this.speed = speed;
-        this.angle = angle;
-    }
+  constructor(x, y, scale, index, img, angle = 0, speed = 0, spawnTimer = 0.8) {
+    this.scale = scale;
+    this.x = x;
+    this.y = y;
+    this.spawnTimer = spawnTimer;
+    this.index = index;
+    this.img = img;
+    this.speed = speed;
+    this.angle = angle;
+  }
 
-    particlesMain() {
-        if(this.spawnTimer < 0) drawImage(ctx, this.img, this.x, this.y, 0, this.scale);
-        else this.spawnTimer -= 0.2 * hzCoef * timeCoef;
-        this.scale -= 0.002 * hzCoef * timeCoef;
+  particlesMain() {
+    if (this.spawnTimer < 0)
+      drawImage(ctx, this.img, this.x, this.y, 0, this.scale);
+    else this.spawnTimer -= 0.2 * hzCoef * timeCoef;
+    this.scale -= 0.002 * hzCoef * timeCoef;
 
-        this.x += this.speed * Math.cos(this.angle) * hzCoef * timeCoef;
-        this.y += this.speed * Math.sin(this.angle) * hzCoef * timeCoef;
-        
-        if(this.scale <= 0){
-            allParticles.splice(this.index, 1);
-            objectDestroyed(this.index, allParticles);
-        }
+    this.x += this.speed * Math.cos(this.angle) * hzCoef * timeCoef;
+    this.y += this.speed * Math.sin(this.angle) * hzCoef * timeCoef;
+
+    if (this.scale <= 0) {
+      allParticles.splice(this.index, 1);
+      objectDestroyed(this.index, allParticles);
     }
+  }
 }
 
 class Enemy {
-    constructor(x, y, speed, index){
-        this.x = x;
-        this.y = y;
-        this.angle = 0;
-        this.speed = speed;
-        this.index = index;
+  constructor(x, y, speed, index) {
+    this.x = x;
+    this.y = y;
+    this.angle = 0;
+    this.speed = speed;
+    this.index = index;
+    this.shootDelay = Math.random() * 20 + 10;
+    this.isSpawned = false;
+    this.spawnerAngle = 0;
+    this.scale = 0;
+    this.spawnerScale = 0;
+    this.interval;
+  }
+
+  enemyMain() {
+    this.angle =
+      calculateAngle(this.x, this.y, xPos, yPos, false) + toRadians(90);
+
+    if (this.isSpawned) {
+      this.x += this.speed * Math.cos(this.angle) * hzCoef * timeCoef;
+      this.y += this.speed * Math.sin(this.angle) * hzCoef * timeCoef;
+      this.shootDelay -= 0.1 * hzCoef * timeCoef;
+
+      if (this.shootDelay <= 0) {
+        bullets.push(
+          new Bullet(
+            this.angle,
+            getCenterX(this.x, player, 0.4),
+            getCenterY(this.y, player, 0.4),
+            1,
+            bullets.length,
+            true
+          )
+        );
         this.shootDelay = Math.random() * 20 + 10;
-        this.isSpawned = false;
-        this.spawnerAngle = 0;
-        this.scale = 0;
-        this.spawnerScale = 0;
-        this.interval;
-    }
+      }
 
-    enemyMain() {
-        this.angle = calculateAngle(this.x, this.y, xPos, yPos, false) + toRadians(90);
-
-        if(this.isSpawned){
-        this.x += this.speed * Math.cos(this.angle) * hzCoef * timeCoef;
-        this.y += this.speed * Math.sin(this.angle) * hzCoef * timeCoef;
-        this.shootDelay -= 0.1 * hzCoef * timeCoef;
-
-        if(this.shootDelay <= 0) {
-            bullets.push(new Bullet(
-            this.angle, 
-            getCenterX(this.x, player, 0.4), 
-            getCenterY(this.y, player, 0.4), 
-            1, bullets.length, true))
-            this.shootDelay = Math.random() * 20 + 10;
-        }
-
-        if(this.spawnerScale > 0) this.scaleDownSpawner();
+      if (this.spawnerScale > 0) this.scaleDownSpawner();
     } else this.renderSpawner();
     drawImage(ctx, player, this.x, this.y, this.angle, this.scale);
 
-        this.detectCollision();
+    this.detectCollision();
+  }
+
+  renderSpawner() {
+    drawImage(
+      ctx,
+      spawner,
+      this.x,
+      this.y,
+      this.spawnerAngle,
+      this.spawnerScale,
+      true
+    );
+
+    this.spawnerAngle += 0.1 * hzCoef * timeCoef;
+    if (this.spawnerScale < 0.15)
+      this.spawnerScale += 0.0015 * hzCoef * timeCoef;
+    else this.scale += 0.005 * hzCoef * timeCoef;
+
+    if (this.scale >= 0.4) this.isSpawned = true;
+  }
+
+  scaleDownSpawner() {
+    drawImage(
+      ctx,
+      spawner,
+      this.x,
+      this.y,
+      this.spawnerAngle,
+      this.spawnerScale,
+      true
+    );
+
+    this.spawnerAngle += 0.1 * hzCoef * timeCoef;
+    if (this.spawnerScale > 0) this.spawnerScale -= 0.002 * hzCoef * timeCoef;
+    else clearInterval(this.interval);
+  }
+
+  detectCollision() {
+    for (let i = 0; i < bullets.length; ++i) {
+      const centerX = getCenterX(this.x, player, 0.4);
+      const centerY = getCenterY(this.y, player, 0.4);
+      if (
+        Math.sqrt(
+          Math.pow(bullets[i].x - centerX, 2) +
+            Math.pow(bullets[i].y - centerY, 2)
+        ) < 20 &&
+        !bullets[i].isEnemy
+      ) {
+        explosion(centerX, centerY, 10, 0.5, 15);
+        enemies.splice(this.index, 1);
+        objectDestroyed(this.index, enemies);
+        bullets[i].destroy();
+      }
     }
-
-    renderSpawner() {
-        drawImage(ctx, spawner, this.x, this.y, this.spawnerAngle, this.spawnerScale, true);
-
-        this.spawnerAngle += 0.1 * hzCoef * timeCoef;
-        if(this.spawnerScale < 0.15) this.spawnerScale += 0.0015 * hzCoef * timeCoef;
-        else this.scale += 0.005 * hzCoef * timeCoef;
-
-        if(this.scale >= 0.4) this.isSpawned = true;
-    }
-
-    scaleDownSpawner() {
-        drawImage(ctx, spawner, this.x, this.y, this.spawnerAngle, this.spawnerScale, true);
-
-        this.spawnerAngle += 0.1 * hzCoef * timeCoef;
-        if(this.spawnerScale > 0) this.spawnerScale -= 0.002 * hzCoef * timeCoef;
-        else clearInterval(this.interval);
-    }
-
-    detectCollision() {
-        for(let i = 0; i < bullets.length; ++i){
-            const centerX = getCenterX(this.x, player, 0.4);
-            const centerY = getCenterY(this.y, player, 0.4);
-            if(Math.sqrt(Math.pow(bullets[i].x - centerX, 2) + Math.pow(bullets[i].y - centerY, 2)) < 20 && !bullets[i].isEnemy){
-                explosion(centerX, centerY, 10, 0.5, 15);
-                enemies.splice(this.index, 1);
-                objectDestroyed(this.index, enemies);
-                bullets[i].destroy();
-            }
-        }
-    }
+  }
 }
